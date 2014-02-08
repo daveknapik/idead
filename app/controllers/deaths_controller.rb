1577456e2de3a4aa0d6019b8e21e9d8d6d2d431a
Dave Knapik <daveknapik@gmail.com>
2014-02-08 14:46:20 -0500

    Added memcached via Dalli gem. Caching current trends and Wikipedia look ups for 1 minute, keeping us within Twitter rate limits

--- app/controllers/deaths_controller.rb
+++ app/controllers/deaths_controller.rb
@@ -5,7 +5,9 @@ class DeathsController < ApplicationController
     twitter = TwitterClient.new
     trends = twitter.trends
 
-    topics_cross_referenced_against_wikipedia = TrendingTopic.cross_reference_wikipedia(trends)
+    topics_cross_referenced_against_wikipedia = Rails.cache.fetch("topics_cross_referenced_against_wikipedia", expires_in: 1.minute) do
+      TrendingTopic.cross_reference_wikipedia(trends)
+    end
 
     @dead = topics_cross_referenced_against_wikipedia[:dead]
     @unknown = topics_cross_referenced_against_wikipedia[:unknown]
