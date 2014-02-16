46ddad27d1d42e644e963b57889caf57d737cba8
Dave Knapik <daveknapik@gmail.com>
2014-02-15 20:19:49 -0500

    Added tests for user.password_required?

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,5 +2,15 @@ FactoryGirl.define do
   factory :user do
     email     Faker::Internet.email
     password  Faker::Internet.password
+
+    factory :user_with_authentications do
+      ignore do
+        authentications_count 1
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:authentication, evaluator.authentications_count, user: user)
+      end
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,25 @@
 require 'spec_helper'
 
 describe User do
+  describe ".password_required?" do
+    it "returns true if the user has no authentications" do
+      user = FactoryGirl.build :user
+      expect(user.password_required?).to be_true
+    end
+
+    it "returns true if the user has a password, whether or not they have authentications" do
+      user_with_authentications = FactoryGirl.build :user_with_authentications, password: Faker::Internet.password
+      expect(user_with_authentications.password_required?).to be_true
+
+      user_without_authentications = FactoryGirl.build :user, password: Faker::Internet.password
+      expect(user_without_authentications.password_required?).to be_true
+    end
+
+    it "returns false if the user has authentications but no password" do
+      user = FactoryGirl.build :user, password: nil
+      user.authentications << FactoryGirl.build(:authentication)
+
+      expect(user.password_required?).to be_false
+    end
+  end
 end
